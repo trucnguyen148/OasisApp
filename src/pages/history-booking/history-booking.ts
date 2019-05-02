2cdf4620ad6b06e3e2e5331adcfeda033168e652
TrucNguyen <trucnguyen@github.com>
2019-05-02 22:14:41 +0300

    updated

--- src/pages/history-booking/history-booking.ts
+++ src/pages/history-booking/history-booking.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ViewController } from 'ionic-angular';
 
 /**
  * Generated class for the HistoryBookingPage page.
@@ -15,7 +15,7 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class HistoryBookingPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public viewCtrl: ViewController) {
   }
 
   schedules = [];
@@ -28,6 +28,9 @@ export class HistoryBookingPage {
         service: 'Eyebrown'
       }
     ]
+  }
+  closeModal(){
+    this.viewCtrl.dismiss();
   }
 
 }
--- src/pages/stylist/stylist.ts
+++ src/pages/stylist/stylist.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ModalController } from 'ionic-angular';
 import { HistoryBookingPage } from '../history-booking/history-booking';
 
 /**
@@ -16,7 +16,7 @@ import { HistoryBookingPage } from '../history-booking/history-booking';
 })
 export class StylistPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public modalCtrl: ModalController) {
   }
   employees = [];
   ionViewDidLoad() {
@@ -53,8 +53,11 @@ export class StylistPage {
       },
     ]
   }
+
+
   goToHistoryBookingPage(){
-    this.navCtrl.push(HistoryBookingPage);
+    let myModal = this.modalCtrl.create(HistoryBookingPage);
+    myModal.present();
   }
 
 }
